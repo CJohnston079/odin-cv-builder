3ab9ca09e6e0584a28b47c6a8d6ceb43fefc8ccb
Chris <chris79gp@outlook.com>
2024-03-25 22:06:41 +0000

    feat: add website, social links and professional summary fieldsets

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,24 @@ function App() {
 						<input name="phone-number" />
 					</label>
 				</fieldset>
+				<fieldset>
+					<h2>Website and social links</h2>
+					<label>
+						Website:
+						<input name="website" />
+					</label>
+					<label>
+						LinkedIn:
+						<input name="linkedin" />
+					</label>
+				</fieldset>
+				<fieldset>
+					<h2>Professional summary</h2>
+					<label>
+						Professional summary:
+						<input name="summary" />
+					</label>
+				</fieldset>
 				<button>Submit</button>
 			</form>
 		</section>
