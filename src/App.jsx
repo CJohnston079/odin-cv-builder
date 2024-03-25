12bc999a415e8ff1ef8eb1a388c7f71a92bc86f0
Chris <chris79gp@outlook.com>
2024-03-25 21:56:54 +0000

    feat: add jsx for contact information in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<section>
+			<form>
+				<fieldset>
+					<h2>Contact information</h2>
+					<label>
+						First name:
+						<input name="forename" />
+					</label>
+					<label>
+						Surname:
+						<input name="surname" />
+					</label>
+					<label>
+						Email address:
+						<input name="email" />
+					</label>
+					<label>
+						Phone number:
+						<input name="phone-number" />
+					</label>
+				</fieldset>
+				<button>Submit</button>
+			</form>
+		</section>
+	);
 }
 
-export default App
+export default App;
