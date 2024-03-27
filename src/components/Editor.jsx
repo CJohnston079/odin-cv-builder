5768bd2e41f72b7711b86ef4b72bd1d101d2ea4f
Chris <chris79gp@outlook.com>
2024-03-27 15:45:32 +0000

    feat: add Fieldset() which returns a fieldset containing Fields as children and refactor fieldsets in Editor() to use Fieldset()

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -7,27 +7,40 @@ function Field({ inputLabel, inputName }) {
 	);
 }
 
+function Fieldset({ heading, fields }) {
+	const fieldElements = fields.map(field => {
+		return <Field key={field.name} inputLabel={field.label} inputName={field.name} />;
+	});
+
+	return (
+		<fieldset>
+			<h2>{heading}</h2>
+			{fieldElements}
+		</fieldset>
+	);
+}
+
 export default function Editor() {
+	const inputs = {
+		contact: [
+			{ label: "First name", name: "forename" },
+			{ label: "Surname", name: "surname" },
+			{ label: "Email address", name: "email" },
+			{ label: "Phone number", name: "phone-number" },
+		],
+		links: [
+			{ label: "Website", name: "website" },
+			{ label: "LinkedIn", name: "linkedin" },
+		],
+		summary: [{ label: "Professional summary", name: "summary" }],
+	};
+
 	return (
 		<section id="editor">
 			<form>
-				<fieldset>
-					<h2>Contact information</h2>
-					<Field inputLabel="First name" inputName="forename" />
-					<Field inputLabel="Surname" inputName="surname" />
-					<Field inputLabel="Email address" inputName="email" />
-					<Field inputLabel="Phone number" inputName="phone-number" />
-				</fieldset>
-				<fieldset>
-					<h2>Website and social links</h2>
-					<Field inputLabel="Website" inputName="website" />
-					<Field inputLabel="LinkedIn" inputName="linkedin" />
-				</fieldset>
-				<fieldset>
-					<h2>Professional summary</h2>
-					<Field inputLabel="Professional summary" inputName="summary" />
-				</fieldset>
-				<button>Submit</button>
+				<Fieldset heading="Contact information" fields={inputs.contact} />
+				<Fieldset heading="Website and social links" fields={inputs.links} />
+				<Fieldset heading="Professional summary" fields={inputs.summary} />
 			</form>
 		</section>
 	);
