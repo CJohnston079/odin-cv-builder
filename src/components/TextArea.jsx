d8f5a2d4eb0b2a07d495a8aa4d6b7ef3496be33b
Chris <chris79gp@outlook.com>
2024-04-11 21:44:17 +0100

    feat: add character count and word count to TextArea components

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 import "../styles/editor/Field.css";
 
 export default function TextArea({ inputName, description }) {
+	const [charCount, setCharCount] = useState(0);
+	const [wordCount, setWordCount] = useState(0);
+
+	const handleInputChange = event => {
+		setCharCount(event.target.value.length);
+		setWordCount(event.target.value.match(/\S+/g)?.length || 0);
+	};
+
 	return (
 		<label>
 			{description}
-			<textarea name={inputName} />
+			<textarea onChange={handleInputChange} name={inputName} />
+			<span className="word-count">{`${charCount} characters, ${wordCount} words`}</span>
 		</label>
 	);
 }
