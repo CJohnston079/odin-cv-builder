a3a188de3fe74b7d0e85bb911599a83d7b1da536
Chris <chris79gp@outlook.com>
2024-04-13 20:58:36 +0100

    refactor: abstract logic for fieldset element from FormSection.jsx to Fieldset.jsx

---
+++ src/components/Fieldset.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+
+import AddFields from "./AddFields";
+import Field from "./Field";
+import TextArea from "./TextArea";
+import ToggleFields from "./ToggleFields";
+
+export default function Fieldset({ fields, isActive }) {
+	const [bonusFieldsShown, setBonusFieldsShown] = useState(false);
+
+	const toggleBonusFields = () => {
+		setBonusFieldsShown(!bonusFieldsShown);
+	};
+
+	const addFields = () => {
+		alert("time to add new fields");
+
+		const parentElement = document.querySelector("add-fields").parentNode;
+		const newDiv = document.createElement("div");
+		newDiv.className = "section-fields";
+		newDiv.textContent = "test";
+		parentElement.appendChild(newDiv);
+	};
+
+	const createField = field => {
+		const Component = field.inputType === "text-area" ? TextArea : Field;
+
+		return (
+			<Component
+				key={field.name}
+				inputType={field.inputType}
+				inputName={field.name}
+				description={field.description}
+			/>
+		);
+	};
+
+	const defaultFields = fields.filter(field => !field.optional);
+	const bonusFields = fields.filter(field => field.optional);
+	const fieldElements = defaultFields.map(createField);
+	const bonusFieldElements = bonusFields.map(createField);
+
+	const accordionCondition = condition => `${condition ? "accordion-active" : "accordion-hidden"}`;
+
+	return (
+		<fieldset className={`accordion ${accordionCondition(isActive)}`}>
+			<div className="section-fields">{fieldElements}</div>
+			<div className={`section-fields ${accordionCondition(bonusFieldsShown)}`}>
+				{bonusFieldElements}
+			</div>
+			{bonusFields.length > 0 && (
+				<ToggleFields toggleFieldsFunc={toggleBonusFields} bonusFieldsShown={bonusFieldsShown} />
+			)}
+			<AddFields addFieldsFunc={addFields} />
+		</fieldset>
+	);
+}
+
+Fieldset.propTypes = {
+	fields: PropTypes.arrayOf(
+		PropTypes.shape({
+			description: PropTypes.string.isRequired,
+			name: PropTypes.string.isRequired,
+		})
+	).isRequired,
+	isActive: PropTypes.bool.isRequired,
+};
--- src/components/FormSection.jsx
+++ src/components/FormSection.jsx
@@ -1,65 +1,13 @@
-import { useState } from "react";
 import PropTypes from "prop-types";
 import SectionHeader from "./SectionHeader";
-import Field from "./Field";
-import TextArea from "./TextArea";
-import ToggleFields from "./ToggleFields";
-import AddFields from "./AddFields";
+import Fieldset from "./Fieldset";
 import "../styles/editor/FormSection.css";
 
 export default function FormSection({ header, fields, isActive, onShow }) {
-	const [bonusFieldsShown, setBonusFieldsShown] = useState(false);
-
-	const toggleBonusFields = () => {
-		setBonusFieldsShown(!bonusFieldsShown);
-	};
-
-	const addFields = () => {
-		alert("time to add new fields");
-
-		const parentElement = document.querySelector("add-fields").parentNode;
-		const newDiv = document.createElement("div");
-		newDiv.className = "section-fields";
-		newDiv.textContent = "test";
-		parentElement.appendChild(newDiv);
-	};
-
-	const createField = field => {
-		const Component = field.inputType === "text-area" ? TextArea : Field;
-
-		return (
-			<Component
-				key={field.name}
-				inputType={field.inputType}
-				inputName={field.name}
-				description={field.description}
-			/>
-		);
-	};
-
-	const defaultFields = fields.filter(field => !field.optional);
-	const bonusFields = fields.filter(field => field.optional);
-	const fieldElements = defaultFields.map(createField);
-	const bonusFieldElements = bonusFields.map(createField);
-
-	const accordionCondition = condition => `${condition ? "accordion-active" : "accordion-hidden"}`;
-
 	return (
 		<div id={header.heading.toLowerCase().replaceAll(" ", "-")} className="form-section">
 			<SectionHeader header={header} isActive={isActive} onShow={onShow} />
-
-			<fieldset className={`accordion ${accordionCondition(isActive)}`}>
-				<div className="section-fields">{fieldElements}</div>
-				<div className={`section-fields ${accordionCondition(bonusFieldsShown)}`}>
-					{bonusFieldElements}
-				</div>
-				{bonusFields.length > 0 && (
-					<ToggleFields toggleFieldsFunc={toggleBonusFields} bonusFieldsShown={bonusFieldsShown} />
-				)}
-				{header.heading !== "Personal details" && header.heading !== "Professional summary" && (
-					<AddFields addFieldsFunc={addFields} />
-				)}
-			</fieldset>
+			<Fieldset fields={fields} isActive={isActive} />
 		</div>
 	);
 }
